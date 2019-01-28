7883f0169c2d3cd52f381d8cd630e1fa56863b3e
Ян Золоторевич <yanzolotorevich@gmail.com>
2019-01-28 20:50:50 +0300

    Practice

--- A-4-Garbage-Collection/Practice.cs
+++ A-4-Garbage-Collection/Practice.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,23 @@ namespace Advenced.Lesson_4
         /// </summary>
         public static void AL4_P1_P2_P3_5_InstanceCounter()
         {
+            for (int i = 0; i<1500000;i++)
+            {
+                using (Counter number = new Counter())
+                {
+                    if (i % 50000 == 0)
+                    {
+                        Console.WriteLine("I равно: " + Counter.number);
+                        Console.WriteLine(GC.GetTotalMemory(false));
+                        GC.Collect();
+                        Console.WriteLine("I равно: " + Counter.number);
+                        Console.WriteLine(GC.GetTotalMemory(false));
+                    }
+                    //number.Dispose();
+                }
+
+
+            }
         }
 
         /// <summary>
@@ -23,6 +41,27 @@ namespace Advenced.Lesson_4
         /// </summary>
         public static void AL4_P4_P5_5_InstanceCounter()
         {
+        }
+        
+        public class Counter : IDisposable
+        {
+            public static int number;
+
+            public Counter()
+            {
+                number++;
+            }
+
+            ~Counter()
+            {
+                //number--;
+                Dispose();
+            }
+            public void Dispose()
+            {
+                GC.SuppressFinalize(this);
+                number--;
+            }
         }
     }
 }
